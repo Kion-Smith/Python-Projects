0a25157f7ae22f3da9da43fb3f329a08bd6c6ae5
Kion Smith <30888048+Kion-Smith@users.noreply.github.com>
2018-07-17 18:55:07 -0500

    Learning about polymorphic functions

--- LearningClasses.py
+++ LearningClasses.py
@@ -1,6 +1,4 @@
-class Person:
-
-    
+class Person(object):
     #I dont think you can create an empty default constructor in python, but 
     # @classmethod can be used for 
     def __init__(self, name=""):
@@ -11,7 +9,7 @@ class Person:
 
 
         
-    def assignName(self,name): 
+    def setName(self,name): 
         tempName = self.name
         self.name = name 
         return("I was "+tempName+ " now I go by "+self.name+".")
@@ -22,19 +20,46 @@ class Person:
             return('I do not have a name')
         else:
             return('Hello my name is '+self.name)
+        
+class Student(object):
+    def __init__(self,name):
+        if not name:
+            self.name = "Null"
+        else:
+            self.name = name
             
+    def setName(self,name):
+        tempName = self.name
+        self.name = name
+        return("Hello I am "+self.name+" and I am a student")
+
+
+def assignName(personType):
+    return personType.name
+        
 
 personObj = Person()
 kionObj = Person("Kion")
+studentObj = Student("Jack")
 
 print(personObj.name)
 print(kionObj.name)
+print(studentObj.name)
 
-print(personObj.assignName("Jeff"))
-print(kionObj.assignName("Null"))
+print("++++++++++++++")
+print(personObj.setName("Jeff"))
+print(kionObj.setName("Null"))
+print("++++++++++++++")
 
+print(studentObj.setName("J A C K"))
 print(personObj.introduction())
 print(kionObj.introduction())
+print("++++++++++++++")
+
+
+print(assignName(personObj))
+print(assignName(kionObj))
+print(assignName(studentObj))
 
 
 
