b8b5db272d9a1e71b4bb8ceac7d37ce32da77a56
Kion-Smith <kion_smith@hotmail.com>
2018-10-31 23:40:57 -0500

    messing with sockets again

--- Sockets/socektTests.py
+++ Sockets/socektTests.py
@@ -1,7 +1,7 @@
 import socket
 
 # The first input of the socket of the is AF INTET which is a ipv4 address and SOCK STREAM refers to the TCP protocols
-curSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+curSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # leanred that DGRAM is unrealible and should be using sock_stream
 
 port = 80
 ip = "8.8.8.8"
@@ -10,5 +10,14 @@ curSocket.connect((ip,port))
 
 # returns current ip and the port number
 print( curSocket.getsockname())
-
 curSocket.close()
+
+#using local host to try and do the same thing
+localSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+localIp = "localhost"
+localPort = 10000
+
+serverAdress = (localIp, localPort)
+localSocket.bind(serverAdress)
+
+print(localSocket.getsockname())
