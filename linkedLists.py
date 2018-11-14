4b47f0aa6b3d110f2b531c3421adc0ae1f4ded8c
Kion-Smith <kion_smith@hotmail.com>
2018-11-13 21:54:57 -0600

    trying to understand data structures better

---
+++ linkedLists.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, num, nextNode):
+        self.num = num
+        self.nextNode = None
+        
+class linkedlist:
+    def __init__(self):
+
+    def inset(self, node):
+        
+    def delete ():
+        
