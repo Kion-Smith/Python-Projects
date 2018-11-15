f62c4125f14932ac543f4bafb3cd01cb5565d7ff
Kion-Smith <kion_smith@hotmail.com>
2018-11-14 23:22:44 -0600

    working some more on this linked list in python

--- linkedLists.py
+++ linkedLists.py
@@ -1,12 +1,21 @@
+#!!!Only compiled not actually tested
 class Node:
-    def __init__(self, num, nextNode):
-        self.num = num
+    def __init__(self, data, nextNode):
+        self.data = data #proably a better naming convetion for python
         self.nextNode = None
         
 class linkedlist:
-    def __init__(self):
+    #no overloaded constructors so using none here
+    def __init__(self, Node=None):
+        self.head = Node
 
     def inset(self, node):
+        if self.head = None:
+            self.head = node
+        else:
+           temp = self.head
+           self.head = node
+           self.head.NextNode = temp
         
     def delete ():
         
