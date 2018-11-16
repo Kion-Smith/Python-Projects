afff2b279c9554fc171c258f438524da7db4108b
Kion Smith <30888048+Kion-Smith@users.noreply.github.com>
2018-11-15 22:37:15 -0600

    adding more functions

--- linkedLists.py
+++ linkedLists.py
@@ -9,13 +9,28 @@ class linkedlist:
     def __init__(self, Node=None):
         self.head = Node
 
-    def inset(self, node):
+    def insetAtFirst(self, node):
         if self.head = None:
             self.head = node
         else:
            temp = self.head
            self.head = node
            self.head.NextNode = temp
-        
-    def delete ():
+
+    def findElement(self,element):
+        #need to think about looping
+
+    #there might be a problem here
+    def delete (self,key):
+        cur = self.hear
+        while cur is not None:
+            if cur.data == key:
+                temp = cur.nextNode
+                cur.nextNode = none
+                
+    def printList(self):
+        cur = self.head
+        while cur is not None:
+            print( cur.data )
+            cur = cur.nextNode
         
